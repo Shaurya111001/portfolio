da1171aaa8b9895dac415ce628ad39ef9a808006
Shaurya111001 <adityasingh111001@gmail.com>
2022-09-20 23:46:29 +0530

    first push

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import React, {useEffect} from "react";
+// import * as THREE from "three";
+
+function App() {
+  useEffect(()=>{
+  },[])
+  return (
+    <div className="App">
+      Hello world
+    </div>
+  );
+}
+
+export default App; 
